548a689be1f27382a5a9ef6d457bdb118e988f04
ginofrancocba <ginofrancocba@gmail.com>
2024-01-04 19:38:49 -0300

    Add Change Header

--- src/views/Header.jsx
+++ src/views/Header.jsx
@@ -6,35 +6,35 @@ import './Header.css';
 function Header() {
     return (
         <div className="fixed-header">
-            <Grid 
+            <Grid
                 container className="container-layout">
-                    <nav className="navbar navbar-expand-lg bg-dark">
-                        <ul>
-                            <Grid>
+                <nav className="navbar navbar-expand-lg bg-dark">
+                    <ul>
+                        <Grid>
                             <li>
-                                <img  className="logo" src='/imgs/logo.jpeg' alt='Logo' />
-                            </li> 
-                            </Grid>
-                            <li>
-                                <Link to="/">Home</Link>
+                                <img className="logo" src='/imgs/logo.jpeg' alt='Logo' />
                             </li>
-                            <li>
-                                <Link to="/profesionales">Profesionales</Link>
-                            </li>
-                            <li>
-                                <Link to="/tratamientos">Tratamientos</Link>
-                            </li>
-                            <li>
-                                <Link to="/aparatologia">Aparatologia</Link>
-                            </li>
-                            <li>
-                                <Link to="/contacto">Contacto</Link>
-                            </li>
-                            <li>
-                                <Link to="/login">Login</Link>
-                            </li>
-                        </ul>
-                    </nav>
+                        </Grid>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/profesionales">Profesionales</Link>
+                        </li>
+                        <li>
+                            <Link to="/tratamientos">Tratamientos</Link>
+                        </li>
+                        <li>
+                            <Link to="/aparatologia">Aparatologia</Link>
+                        </li>
+                        <li>
+                            <Link to="/contacto">Contacto</Link>
+                        </li>
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                    </ul>
+                </nav>
             </Grid>
             <Outlet />
         </div>
